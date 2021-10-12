63f52ffab55411a96cb8f8f2ee9410071b7688a1
ankit <ankitforstudy@gmail.com>
2021-10-12 14:29:39 +0530

    added more logic for authentication and otp validation

---
+++ src/main/java/com/example/springsecuritymfaclientdemo/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.example.springsecuritymfaclientdemo.auth.filter;
+
+import com.example.springsecuritymfaclientdemo.auth.authentication.UsernameOtpAuthentication;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.key}")
+    private String signingKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwtToken = request.getHeader("Authorization");
+        Claims claims = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(signingKey.getBytes(StandardCharsets.UTF_8))).
+                build().parseClaimsJwt(jwtToken).getBody();
+        Authentication authentication = new UsernameOtpAuthentication((String)claims.get("username"),"", List.of(new SimpleGrantedAuthority("user")));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().equals("/login");
+    }
+}
